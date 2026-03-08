b9f36a6947b7817e0b2a00abe92d06c66a4463bc
anjalngm-crypto <anjalngm@gmail.com>
2026-03-08 04:52:52 +0000

    completed day 1

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,8 @@
+const midnight=12
+console.log(midnight);
+// midnight= 6 value never be updated due to constant
+let week="7 Days"
+var month= "30 Days"
+year="365 Days"
+let leapYear;//undefined value it stored
+console.table([midnight,week,month,year,leapYear]);
