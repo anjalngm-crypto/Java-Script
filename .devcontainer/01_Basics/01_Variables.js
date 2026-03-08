082ef42607c02f518b8002a5f7e7f3c3f772e440
anjalngm-crypto <anjalngm@gmail.com>
2026-03-08 05:50:18 +0000

    datatype completed

--- .devcontainer/01_Basics/01_Variables.js
+++ .devcontainer/01_Basics/01_Variables.js
@@ -3,6 +3,10 @@ console.log(midnight);
 // midnight= 6 value never be updated due to constant
 let week="7 Days"
 var month= "30 Days"
+/*
+prefer not to use var 
+because of issue in block and functional scope. 
+*/
 year="365 Days"
 let leapYear;//undefined value it stored
 console.table([midnight,week,month,year,leapYear]);
---
+++ .devcontainer/01_Basics/02_Datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using node.js ,not browser
+
+console.log(3+3)
+
+console.log("Anjal")
+
+let name="Anjal"//String
+let age=20//number
+let isLoggedIn=false//boolean
+ 
+
+
+// PRIMITIVE DATA TYPE
+
+
+// number=> 2 ^ 53
+//bigint
+// string => single or double qoutes
+// boolean => true or false
+// null => Standalone value it is datatype/representation of empty value it is not undefined
+// undefined => the value is not assigned currently
+// symbol => use for uniqueness
+
+// OBJECT
+
+console.log(typeof "Anjal"); //help to get know about the type of data
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // object is o/p of this
